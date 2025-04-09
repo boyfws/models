88a54ea5e8c93b39c95689e4089dbc064bd752f8
Daniel <minkin.d.d@gmail.com>
2025-04-10 00:40:30 +0300

    Docs added

--- Trees/Node.py
+++ Trees/Node.py
@@ -1,6 +1,14 @@
 import numpy as np
+from typing import Union
 
 class Condition:
+    feature_num: int
+    t: float
+
+    """
+    A class that determines whether the data 
+    belongs to one of the groups i.e. defines a split 
+    """
     def __init__(self, feature_num: int, t: float) -> None:
         self.feature_num = feature_num
         self.t = t
@@ -10,6 +18,15 @@ class Condition:
 
 
 class Node:
+    left_node: Union["Node", None]
+    right_node: Union["Node", None]
+    value: Union[int, float, np.ndarray]
+    condition: "Condition"
+    """"
+    This class represents a building block of a tree, 
+    depending on the context it can both refer 
+    to other nodes and store a certain value in itself
+    """
     def __init__(self):
         self.left_node = None # False cond
         self.right_node = None # True cond
@@ -19,6 +36,10 @@ class Node:
         self.condition = None
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Recursively called function, is called until
+        we come to a node that does not reference any others
+        """
         if self.left_node is None or self.right_node is None:
             return np.array([self.value] * X.shape[0], dtype=np.float32)
 
